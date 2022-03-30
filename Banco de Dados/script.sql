9199d5d8f92ba3f8fe4f5acf59dd2685cd0a4b26
joohzinato <joao.sobral@sptech.school>
2022-03-30 00:12:22 -0300

    banco de dados atualiado

--- Banco de Dados/script.sql
+++ Banco de Dados/script.sql
@@ -14,19 +14,52 @@ cepEmpresa char(8)
 
 create table Sensor (
 idSensor int primary key auto_increment,
+tipo varchar(20), constraint chkTipo check (tipo in('temperatura')),
+codigo int,
+data_instalacao date
+);
+
+
+create table Dados (
+idDados int,
+dtH datetime,
 temperatura double,
-cordLatAlt varchar(25),
-dhH datetime,
+altLog varchar(30),
 fkEmpresa int,
-foreign key (fkEmpresa) references Empresa(idEmpresa)
+fkSensor int,
+foreign key (fkEmpresa) references Empresa(idEmpresa),
+foreign key (fkSensor) references Sensor(idSensor)
 );
 
 insert into Empresa (cnpj, nomeEmpresa, emailEmpresa, bairroEmpresa, cidadeEmpresa, cepEmpresa) values
-('13347016000117', 'Brandão', 'brandao@sptech.school', 'Brandão', 'Brandão', '07540450');
+('02916265004002', 'Friboi', 'friboi@friboi.com', 'Bairro Friboi', 'Cidade Friboi', '03978450'),
+('02916475004002', 'Swift', 'swift@swift.com', 'Bairro Swift', 'Cidade Swift', '03978340');
 
-insert into Sensor (temperatura, cordLatAlt, dhH, fkEmpresa) values
-(03.90 , '22-22-33 33-33-44' , '2001-09-11 13:09:26', 1);
+insert into Sensor (tipo, codigo, data_instalacao) values
+('temperatura', 30949, '2020-04-19'),
+('temperatura', 30950, '2020-04-20'),
+('temperatura', 30951, '2020-04-20'),
+('temperatura', 30952, '2020-04-20');
 
-select e.*, s.* from Empresa as e
-	join Sensor as s
-		on s.fkEmpresa = e.idEmpresa;
+insert into Dados (dtH, temperatura, altLog, fkEmpresa, fkSensor) values
+('2022-09-11 13:09:26','30.4', 'x-100 y-400',  1, 1),
+('2022-09-11 14:09:26','30.7', 'x-848 y-400',  1, 1),
+('2022-09-11 15:09:26','30.9', 'x-100 y-848',  1, 3),
+('2022-09-11 16:09:26', '30.12', 'x-848 y-837', 2, 2),
+('2022-09-11 18:09:26','30.15', 'x-100 y-363',  2, 2),
+('2022-09-11 14:09:26','30.12', 'x-100 y-400',  2, 4);
+
+select concat(Empresa.nomeEmpresa, ' - ', Empresa.cnpj) as 'Nome Empresa + CNPJ', 
+concat('Bairro: ',Empresa.bairroEmpresa, ' -- Cidade: ',Empresa.cidadeEmpresa,' -- CEP: ', Empresa.cepEmpresa) as 'Bairro, Cidade e CEP', 
+concat(Sensor.codigo, ' - ' ,Sensor.idSensor) as 'Código do Sensor + ID', 
+concat(Dados.temperatura, ' °C'), 
+Dados.dtH as 'Data e Horário do dado' from Dados 
+	join Sensor on Dados.fkSensor = Sensor.idSensor
+		join Empresa on Dados.fkEmpresa = Empresa.idEmpresa
+			where Empresa.nomeEmpresa = 'Swift' or Empresa.nomeEmpresa = 'Friboi';
+        
+
+
+
+
+
